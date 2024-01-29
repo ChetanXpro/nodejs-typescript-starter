76d6bf3182484e072d9565b221367e503802ded7
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-01-29 23:24:15 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+import { findCurseWordsTimestamp } from './service/ai.service'
+import { createTranscription } from './service/transcription.service'
 import dotenv from 'dotenv'
 dotenv.config()
 
+interface IArgs {
+	absolutePathToVideo: string
+}
+
+const run = async ({ absolutePathToVideo }: IArgs) => {
+	// TODO: Create .srt for given video
+	// const createTranscriptionResult = await createTranscription(absolutePathToVideo)
+	// console.log(createTranscriptionResult)
+
+	const srt_content = findCurseWordsTimestamp({
+		srt_content: 'Yoooo',
+	})
+
+	// TODO: Check in .srt if there are any bad words , and if exist then return them
+	// TODO: Create a new .srt file with bad words replaced with beep sound
+	// TODO: Add new .srt to video
+}
+
+run({
+	absolutePathToVideo: '/Users/chetan/Developer/code/video-scanner/sample.mp4',
+})
